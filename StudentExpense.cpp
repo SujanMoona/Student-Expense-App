064e1468622bc1571a14dfee82554ccdd1c5b01c
Sujan <Sujanatwork654@gmail.com>
2025-09-29 23:16:44 +0530

    Day 1 Complete - View, basic structure

---
+++ StudentExpense.cpp
@@ -0,0 +1,142 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<stack>
+#include<ctime> // Library for Date & Time 
+using namespace std;
+
+
+typedef struct student{
+string name;
+int id
+
+}user;
+
+typedef struct Expense {
+    int id;                // Unique ID
+    string date;           // Format: DD-MM-YYYY
+    string category;       // Food, Travel, etc.
+    string description;    // Optional
+    double amount;
+}Expense;
+
+
+class functions{
+private:
+      int  expenseCount=0;// sr.no
+      Expense expense[5];
+      stack<Expense> undo,redo;//for undo redo 
+      
+public:
+
+    Expense input();
+    void addExpense();         // 1. Add Expense
+    void viewExpense();       // 2. View Expenses
+    void searchExpenseByMoney();  // 3. Search Expense (By Money)
+    void searchExpenseByDate(); // 4. Search Expense (By Date)  
+    void sortExpense();       // 5. Sort Expenses
+    void deleteExpense();      // 6. Delete Expense
+    void undoLastAction();     // 7. Undo Last Action
+    void viewRecentExpenses(); // 8. Recent Expenses
+    
+};
+
+
+Expense functions::input(){
+    Expense expense;
+    expense.id = expenseCount+1;
+    cout<<"Enter the Category";
+    cin>>expense.category;
+    cout<<"Enter the description:";
+    getline(cin,expense.description);
+    cout<<"Enter the amount :";
+    cin>>expense.amount;
+    expenseCount++;
+    undo.push(expense);//for keep the privious actions;
+    return expense;
+
+}
+void functions::addExpense(){
+    expense[expenseCount]=input();// input
+}
+
+void functions::viewExpense(){
+    if (expenseCount == 0){
+        cout<<"No Expenses Found"<<endl;
+    }else {
+        cout<<"----------- Expenses ------------"<<endl;
+        cout<<"Id\tDate\tCategory\tAmount\tDescription"<<endl;
+        cout<<"---------------------------------"<<endl;
+
+        for ( int i = 0; i < expenseCount ; i++){
+            cout<<expense[i].id<<"\t";
+            cout<<expense[i].date<<"\t";
+            cout<<expense[i].category<<"\t";
+            cout<<expense[i].amount<<"\t";
+            cout<<expense[i].description;
+        }
+        cout<<endl;
+    }
+}
+
+
+
+
+void functions::undoLastAction(){
+    if(undo.empty()){
+     cout<<"Undo is not possible"<<endl;
+    }else{
+     redo.push(undo.top());
+     undo.pop();
+    }
+}
+void functions::deleteExpense(){
+    expenseCount--;
+}
+int main() {
+  
+
+    functions f;
+    int choice;
+    do{
+        cout << "\n===== Expense Manager =====\n";
+        cout << "1. Add Expense\n2. View Expenses\n3. Search Expense (By Money)\n";
+        cout << "4. Search Expense (By Date)\n5. Sort Expenses\n6. Delete Expense\n";
+        cout << "7. Undo Last Action\n8. Recent Expenses\n0. Save & Exit\n";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            f.addExpense();
+            break;
+        case 2: 
+            f.viewExpense();
+            break;
+        case 3:
+            f.searchExpenseByMoney();
+            break;
+        case 4:
+            f.searchExpenseByDate();
+            break;
+        case 5:
+            f.sortExpense();
+            break;
+        case 6:
+            f.deleteExpense();
+            break;
+        case 7:
+            f.undoLastAction();
+            break;
+        case 8:
+            f.viewRecentExpenses();
+            break;
+        case 0:
+            cout<<"Exiting...\n";
+            break;            
+        default:
+            cout<<"Invalid Input! Enter Again\n";
+            break;
+        }
+    } while (choice != 0);
+}
