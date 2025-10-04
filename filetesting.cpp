62536a0e387f57f714930dc8cce23f923420bad3
Samarth Mungase <mungasesamarth326@gmail.com>
2025-10-04 14:46:31 +0530

    User login component

--- filetesting.cpp
+++ filetesting.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<fstream>
+#include<conio.h>
 #include<string>
 
 using namespace std;
@@ -18,11 +19,70 @@ struct User{
 };
 
 class account{
-    string user_name;
+    string user_name,password;
     string uname, pass;
+     User u;
+     bool signup=false,l=false;
+     bool user_exist=false;
     
 
 public:
+
+
+void login_menu(){
+   
+    int choice=0;
+    while(true){
+        system("cls");
+        cout<<(choice==0?">>" :" ")<<"Login"<<endl; 
+        cout<<(choice==1?">>" :" ")<<"Signup"<<endl;
+    
+
+        char key=_getch();
+        if(key==72){
+                choice=(choice+1)%2;
+        }else if(key==80){
+                choice=(choice+1)%2;
+        }else if(key==13){
+                break;
+        }
+
+    }
+    switch(choice+1){
+          
+        case 1:{
+            system("cls");
+            login();
+            l=findUser();
+            if(findUser()){
+                cout<<"welcome "<<u.user_name<<endl;
+                u.user_name=u.user_name+".txt";
+                ofstream file(u.user_name,ios ::in | ios::out);//to read or write accordin to (<< ,>>);
+        //now this can alloe you to store and write the daily expenses 
+            }
+            else{
+                cout<<"user name not found!"<<endl;
+                login_menu();
+            }
+            break;
+        }
+        case 2:{
+                system("cls");
+                create_file();
+            
+            break;
+        }
+    }
+
+
+    if(signup || l){
+    cout<<(l? "login succesful":"signup succesful")<<endl;
+        
+    }
+
+  
+
+}
 void login(){
     cout<<"Enter username:";
     cin>>uname;
@@ -30,45 +90,77 @@ void login(){
     cin>>pass;
 }
 
-void createFile(){
+void create_file(){
  cout<<"Enter username: ";
  cin>>user_name;
- user_name=user_name+".txt";
- ofstream file(user_name);
+ ifstream User_info("users.txt");// to read the file of user and password
+ while (User_info>>u.user_name >> u.password){ 
+    //to avoid duplicate username;
+      if(user_name==u.user_name){
+        cout<<"user already exist"<<endl;
+        login_menu();
+    }
+}
+    User_info.close();
+    cout<<"Enter password: ";
+    cin>>password;
+
+ //user_name=user_name+".txt";
+ ofstream file(user_name+".txt");
  file<<user_name;
  file.close();
+
+ ofstream file1("users.txt",ios::app );
+ file1<<user_name<<" "<<password<<endl;
+ file1.close();
+ 
+ cout<<"signup Successful!"<<endl;
+ 
 }
 
 
+//to findout is the user exist or not
 
 bool findUser(){
- User u;
- 
+
  ifstream User_info("users.txt");// to read the file of user and password
- login();//user input
- 
  while (User_info>>u.user_name >> u.password){ 
-    if ( uname == u.user_name && pass == u.password ){
+    //to avoid duplicate username;
+      if(uname==u.user_name && pass!=u.password){
+     
+       user_exist=true;
+    } 
+     if ( uname == u.user_name && pass == u.password ){
        return true;   
     }
+  
  }
  return false;
  
 }
+//this is main funtion of the account login or signup
 
 void account_main(){
     login();
-    
+    l=findUser();
+    if(findUser()){
+        cout<<"welcome "<<u.user_name<<endl;
+        u.user_name=u.user_name+".txt";
+        ofstream file(u.user_name,ios ::in | ios::out);//to read or write accordin to (<< ,>>);
+        //now this can alloe you to store and write the daily expenses 
 
+    }else{
+        cout<<"invalid username and password"<<endl;
+         create_file();
+    }
 }
-
-
 };
 
 
 
 int main(){
-
+account a;
+a.login_menu();
 return 0;
 
  
